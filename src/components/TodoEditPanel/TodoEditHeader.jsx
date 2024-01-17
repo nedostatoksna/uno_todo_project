113448f766fda518a630db42f2b52b6b7f162ddf
Margarita Zhentsova <mzhentsova@gmail.com>
2024-01-17 12:46:34 +0400

    new component Title (from todoEditHeader)

--- src/components/TodoEditPanel/TodoEditHeader.jsx
+++ src/components/TodoEditPanel/TodoEditHeader.jsx
@@ -1,33 +1,17 @@
-import React, { useCallback, useContext, useEffect, useRef } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AppContext } from "../../context/context";
-import styled, { css } from "styled-components";
 import Checkbox from "../../ui/Checkbox.jsx";
 import { 
     changeCompleted, 
-    changeImportant, 
-    changeTitle } from "../../store/actionCreators/dataListActionCreators.js";
-import { toggleEditingTitle } from "../../store/actionCreators/todoPanelActionCreators";
+    changeImportant } from "../../store/actionCreators/dataListActionCreators.js";
 import FlexRowWrapper from "../../ui/FlexRowWrapper.jsx";
+import Title from "./Title.jsx";
 
 const TodoEditHeader = ({ todo }) => {
 
-    const titleInputFocus = useRef(null);
     const dispatch = useDispatch();
-    const context = useContext(AppContext);
     const activeTodoId = useSelector(state => state.todoPanelUI.activeTodoId);
     const activeListId = useSelector(state => state.todoListUI.activeListId);
-    const isEditingTitle = useSelector(state => state.todoPanelUI.isEditingTitle);
-
-    const cashedPreEdit = useCallback(() => {
-        dispatch(toggleEditingTitle());
-    }, [dispatch]);
-
-    useEffect(() => {
-        const input = titleInputFocus.current;
-        input.addEventListener("focus", cashedPreEdit);
-        return () => input.removeEventListener("focus", cashedPreEdit);
-    }, [cashedPreEdit]);
 
     return (
         <FlexRowWrapper 
@@ -46,28 +30,7 @@ const TodoEditHeader = ({ todo }) => {
                     todoId: activeTodoId, 
                     isCompleted: !todo.isCompleted }))}}
             />
-            <StyledTitleWrapper 
-                $mode={context.mode} 
-                $isEditing={isEditingTitle ? true : false}
-            >
-                { isEditingTitle && <StyledLabel 
-                                        htmlFor="titleInput" 
-                                        $mode={context.mode}
-                                    >
-                                        Task Name
-                                    </StyledLabel> }
-                <StyledTodoTitle 
-                    ref={titleInputFocus} 
-                    $mode={context.mode} 
-                    $isEditing={isEditingTitle ? true : false}
-                    value={todo.title} 
-                    onChange={(e) => dispatch(changeTitle({ 
-                        listId: activeListId, 
-                        todoId: activeTodoId, 
-                        todoTitle: e.target.value }))} 
-                    id="titleInput"
-                />
-            </StyledTitleWrapper>
+            <Title todo={todo} />
             <Checkbox 
                 labelStarGrey
                 starChecked 
@@ -84,57 +47,5 @@ const TodoEditHeader = ({ todo }) => {
 
 export default TodoEditHeader;
  
-const StyledTodoTitle = styled.input`
-    font-size: 22px;
-    line-height: 28px; 
-    margin: 0px 10px;
-    width: calc(20vw - 140px);
-    background-color: transparent;
-    ${props => props.$isEditing && css `
-        margin: 0px;
-    `}
-    ${props => props.$mode === "Light" && css`
-        color: var(--black);
-    `} 
-    ${props => props.$mode === "Dark" && css`
-        color: var(--dark-mode-white-text);
-    `} 
-`;
-const StyledTitleWrapper = styled.div`
-    font-size: 22px;
-    line-height: 28px; 
-    margin: 0px 10px;
-    padding: 8px 16px;
-    width: calc(20vw - 120px);
-    ${props => props.$isEditing && css `
-        border-bottom: 2px solid;
-    `}
-    ${props => props.$mode === "Light" && css `
-        color: var(--black);
-        background-color: var(--white);
-
-        ${props => props.$isEditing && css `
-            border-color: var(--primary-purple);
-            background-color: var(--white);
-        `}
-    `}
-    ${props => props.$mode === "Dark" && css `
-        color: var(--dark-mode-white-text);
-        background-color: var(--dark-mode-background);
-
-        ${props => props.$isEditing && css `
-            border-color: var(--dark-mode-primary-purple);
-            background-color: var(--dark-grayish-lavender-variant);
-        `}
-    `}
-`;
-const StyledLabel = styled.label` 
-    ${props => props.$mode === "Light" && css`
-        color: var(--primary-purple);
-    `} 
-    ${props => props.$mode === "Dark" && css`
-        color: var(--dark-mode-primary-purple);
-    `} 
-`;
 
 
